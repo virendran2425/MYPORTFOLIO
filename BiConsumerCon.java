2c0f0f2d98fc7e42722f667c7f37b2f0d8968045
virendran2425 <virendran2425@gmail.com>
2025-09-26 22:29:20 +0530

    Delete BiConsumerCon.java

--- BiConsumerCon.java
+++
@@ -1,9 +0,0 @@
-package java8;
-import java.util.function.BiConsumer;
-
-public class BiConsumerCon {
-    public static void main(String[] args) {
-        BiConsumer<String,String> Bic = (V, N) -> System.out.println(V+"==="+N);
-        Bic.accept("Virendra", "Viru");
-    }
-}
